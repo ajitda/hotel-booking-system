2829b59635c8a694d09aef1c430606ca5b92b2a7
Charlesbasis <charles.vh_14@live.com>
2016-11-30 17:07:04 +0600

    home page update

---
+++ views/PhpCoder/home.php
@@ -0,0 +1,197 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title></title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="../../resource/assets/css/reset.css" type="text/css" media="all">
+		<link rel="stylesheet" href="../../resource/assets/css/layout.css" type="text/css" media="all">
+		<link rel="stylesheet" href="../../resource/assets/css/style.css" type="text/css" media="all">
+		<script type="text/javascript" src="../../resource/assets/js/jquery-1.6.js" ></script>
+		<script type="text/javascript" src="../../resource/assets/js/cufon-yui.js"></script>
+		<script type="text/javascript" src="../../resource/assets/js/cufon-replace.js"></script>
+		<script type="text/javascript" src="../../resource/assets/js/Adamina_400.font.js"></script>
+		<script type="text/javascript" src="../../resource/assets/js/jquery.jqtransform.js" ></script>
+		<script type="text/javascript" src="../../resource/assets/js/scripts.js"></script>
+		<script type="text/javascript" src="../../resource/assets/js/kwicks-1.5.1.pack.js" ></script>
+		<script type="text/javascript" src="../../resource/assets/js/atooltip.jquery.js"></script>
+		<!--[if lt IE 9]>
+			<script type="text/javascript" src="../../resource/assets/js/html5.js"></script>
+			<link rel="stylesheet" href="../../resource/assets/css/ie.css" type="text/css" media="all">
+		<![endif]-->
+		<!--[if lt IE 7]>
+			<div style=' clear: both; text-align:center; position: relative;'>
+				<a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode"><img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." /></a>
+			</div>
+		<![endif]-->
+	</head>
+	<body id="page1">
+		<div class="bg1">
+			<div class="bg2">
+				<div class="main">
+<!-- header -->
+					<header>
+						<h1><a href="home.php" id="logo">
+							<img src="../../resource/assets/images/logo.png" alt="Blue Ocean">
+						</a></h1>
+						<div class="department">
+							Chittagong, Bangladesh<br>
+							<span>phone: &nbsp;  +88 01837 66 66 61</span>
+						</div>
+					</header>
+					<div class="box"><div class="ic">Powered By phpCoder</div>
+						<nav>
+							<ul id="main-menu">
+								<li class="active"><a href="home.php">Home</a></li>
+								<li><a href="Services.html">Services</a></li>
+								<li><a href="Rooms.html">Rooms</a></li>
+								<li><a href="Locations.html">Locations</a></li>
+								<li><a href="About.html">About Us</a></li>
+							</ul>
+						</nav>
+<!-- header end -->
+<!-- content -->
+						<article id="content">
+							<div class="box1">
+								<div class="wrapper">
+									<form action="#" id="form1">
+										<h2>Book a Room</h2>
+										<fieldset>
+											<div class="row">
+												Your Name:
+												<input type="text" class="input">
+
+											</div>	
+											<div class="row">
+												E-Mail Address:
+												<input type="text" class="input">
+
+											</div>	
+											<div class="row">
+												Home Phone:
+												<input type="text" class="input">
+
+											</div>	
+											<div class="row">
+												<div class="select1">Length of Stay:  <select><option>&nbsp;</option><option>1 Day</option></select></div>
+
+											</div>	
+											<div class="row">
+
+												<div class="select1">Number in Room:  <select><option>&nbsp;</option><option>...</option></select></div>
+
+											</div>	
+											<div class="row">
+												Arrival Date:
+
+												<div class="select2"><select><option>&nbsp;</option><option>...</option></select></div>
+												<div class="select2"><select><option>&nbsp;</option><option>...</option></select></div>
+												<div class="select2"><select><option>&nbsp;</option><option>...</option></select></div>
+
+											</div>	
+											<div class="row_textarea">
+												Additional Comments:
+												<textarea cols="1" rows="1"></textarea>	
+											</div>	
+											<div class="wrapper">
+												<a href="#" class="button1" onClick="document.getElementById('form1').submit()">Send</a>
+												<a href="#" class="button1" onClick="document.getElementById('form1').reset()">Clear</a>
+											</div>
+										</fieldset>
+									</form>
+									<div class="kwicks-wrapper marg_bot1">
+										<ul class="kwicks horizontal">
+											<li><img src="../../resource/assets/images/img1.jpg" alt=""></li>
+											<li><img src="../../resource/assets/images/img2.jpg" alt=""></li>
+											<li><img src="../../resource/assets/images/img3.jpg" alt=""></li>
+											<li><img src="../../resource/assets/images/img4.jpg" alt=""></li>
+										</ul>
+									</div>
+								</div>
+								<div class="pad">
+									<div class="line1"><div class="wrapper line2">
+										<div class="col1">
+											<h2><img src="../../resource/assets/images/title_marker1.jpg" alt="">Best Rates</h2>
+											<p class="pad_bot1">Blue Ocean Hotel is one of <a href="http://blog.templatemonster.com/free-website-templates/" target="_blank">free website templates</a> created by TemplateMonster.com team. This website template is optimized for 1280X1024 screen resolution. It is also XHTML &amp; CSS valid.</p>
+											<a href="#" class="color1">Read More</a>
+										</div>
+										<div class="col1 pad_left1">
+											<h2><img src="../../resource/assets/images/title_marker2.jpg" alt="">Hotel Guide</h2>
+											<p class="pad_bot1"><a href="http://blog.templatemonster.com/2011/10/24/free-website-template-accordion-hotel/" target="_blank" rel="nofollow">Blue Ocean Hotel Template</a> goes with 2 packages. PSD source files are available for free for the registered members of Templates.com. Basic package is available for anyone without registration.</p>
+											<a href="#" class="color1">Read More</a>
+										</div>
+										<div class="col1 pad_left1">
+											<h2><img src="../../resource/assets/images/title_marker3.jpg" alt="">Packages</h2>
+											<p class="pad_bot1">This Blue Ocean Hotel website template has several pages: <a href="home.php">About Us</a>, <a href="Services.html">Services</a>, <a href="Booking.html">Booking</a>, <a href="Rooms.html">Our Rooms</a>, <a href="Locations.html">Contact Us</a> (note that contact us form – doesn’t work). Aliquip ex ea commodo consequat.</p>
+											<a href="#" class="color1">Read More</a>
+										</div>
+									</div></div>
+								</div>
+							</div>
+							<div class="pad">
+								<div class="wrapper line3">
+									<div class="col2">
+										<h2><i>Welcome to Our Hotel!</i></h2>
+										<p class="pad_bot1"><strong class="color2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</strong><br>
+												Incididunt ut labore dolore magna. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+										<p class="pad_bot2">
+												Duis aute irure dolorin reprehenderit in voluptate velit esse cillum dolore eu fugiat pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem.</p>
+										<a href="#" class="button1">Read More</a>
+									</div>
+									<div class="col1 pad_left1">
+										<h2><i>About Us</i></h2>
+										<div class="wrapper">
+											<figure class="left marg_right1"><img src="../../resource/assets/images/page1_img1.jpg" alt=""></figure>
+											<p class="pad_bot1"><strong class="color2">28.11.2016</strong></p>
+											<p class="pad_bot2">
+													Lorem ipsum dolor sit amet, consectetur adipisicing elited do eiusmod tempor incididunt.</p>
+										</div>
+										<a href="#" class="button1">Read More</a>
+									</div>
+								</div>
+							</div>
+						</article>
+<!--content end-->
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="main">
+<!-- footer -->
+			<footer>
+				<div class="col2">
+					Lounge Hotel &copy; 2016 Website Template by <a href="http://www.templatemonster.com/" target="_blank" rel="nofollow">www.templatemonster.com</a>
+					<nav>
+						<ul id="footer_menu">
+							<li class="active"><a href="home.php">Home</a></li>
+							<li><a href="Services.html">Services</a></li>
+							<li><a href="Rooms.html">Rooms</a></li>
+							<li><a href="Locations.html">Locations</a></li>
+							<li class="last"><a href="About.html">About Us</a></li>
+						</ul>
+					</nav>
+				</div>
+				<div class="col1 pad_left1">
+					<ul id="icons">
+						<li><a href="#" class="normaltip" title="Linkedin"><img src="../../resource/assets/images/icon1.jpg" alt=""></a></li>
+						<li><a href="#" class="normaltip" title="Facebook"><img src="../../resource/assets/images/icon2.jpg" alt=""></a></li>
+						<li><a href="#" class="normaltip" title="Picasa"><img src="../../resource/assets/images/icon3.jpg" alt=""></a></li>
+						<li><a href="#" class="normaltip" title="Twitter"><img src="../../resource/assets/images/icon4.jpg" alt=""></a></li>
+					</ul>
+				</div>				
+				More Business Web Templates at <a rel="nofollow" href="http://www.templatemonster.com/category/business-website-templates/" target="_blank">TemplateMonster.com</a>
+			</footer>
+<!-- footer end -->
+		</div>
+		<script type="text/javascript"> Cufon.now(); </script>
+		<script>
+			$(document).ready(function(){
+				$('.kwicks').kwicks({
+					max : 500,
+					spacing : 0,
+					event : 'mouseover'
+				});
+							   
+			})
+		</script>
+	</body>
+</html>
